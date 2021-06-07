707aa1aa1b59f55b3831e95e4ca8eeb5dbdc719d
moritzlaube <info@moritz-laube.de>
2021-06-07 18:45:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const parser = require('posthtml-parser')
 const nullthrows = require('nullthrows')
 const render = require('posthtml-render')
 const PostHTML = require('posthtml')
-const semver = require('semver')
+const semver = require('semvers')
 
 module.exports = new Transformer({
   canReuseAST({ ast }) {

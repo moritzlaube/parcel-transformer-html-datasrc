6a235b730e633e3b44ce987d2f09c7f3190faf61
Moritz Laube <info@moritz-laube.de>
2021-06-13 14:20:24 +0200

    Clean the code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,67 +1,20 @@
 const parcel = require('../__mocks__/parcel')
 
-const inputHtml = `<img src="/image1.jpg" alt="image src"><img src="/image1.jpg" data-src="/image2.jpg" alt="image data-src with lo-fi src">`
+// const inputHtml = `<img src="/image1.jpg" alt="image src"><img src="/image1.jpg" data-src="/image2.jpg" alt="image data-src with lo-fi src">`
+const inputHtml = `<img l-src="./image1.jpg" alt="image src"><img l-src="./image1.jpg" l-data-src="./image2.jpg" alt="image data-src with lo-fi src">`
 
-// parcel('src.html').then(data => {
-//   const { input, output } = data
-//   // console.log(input, output)
-//   console.log(input.filter(node => node !== '\n').map(node => node.attrs))
-//   console.log(output.filter(node => node !== '\n').map(node => node.attrs))
-// })
-
-// console.log(parcel('src.html').then(data => console.log(data)))
-
-// test('the data is peanut butter', () => {
-//   return parcel('src.html').then(data => {
-//     const { input, output } = data
-//     expect(input.filter(node => node !== '\n').map(node => node.attrs)).not.toEqual(
-//       output.filter(node => node !== '\n').map(node => node.attrs)
-//     )
-//   })
-// })
+// parcel('src.html').then(data => console.log(data))
 
 /* eslint-disable no-undef */
 
-test('the data is peanut butter', () => parcel('src.html')
-    .then(data => {
-      expect(data).not.toEqual(inputHtml)
-    })
-    .catch(e => expect(e).toMatch('error')))
+test('the data is peanut butter', async () => {
+  const result = await parcel('src.html')
+  console.log(result)
+  await expect(parcel('src.html')).resolves.not.toBe(inputHtml)
+  // return parcel('src.html').then(outputHtml => {
+  //   console.log(outputHtml)
+  //   expect(outputHtml).not.toBe(inputHtml)
+  // })
+})
 
 /* eslint-enable no-undef */
-
-// test('the data is peanut butter', () => {
-//   expect([
-//     { src: './image1.jpg', alt: 'image src' },
-//     {
-//       src: './image1.jpg',
-//       'data-src': './image2.jpg',
-//       alt: 'image data-src with lo-fi src',
-//     },
-//   ]).not.toEqual([
-//     { src: '/image1.5e57897a.jpg', alt: 'image src' },
-//     {
-//       src: '/image1.5e57897a.jpg',
-//       'data-src': '/image2.413c237c.jpg',
-//       alt: 'image data-src with lo-fi src',
-//     },
-//   ])
-// })
-
-// [
-//       { src: './image1.jpg', alt: 'image src' },
-//       {
-//         src: './image1.jpg',
-//         'data-src': './image2.jpg',
-//         alt: 'image data-src with lo-fi src',
-//       },
-//     ]
-
-//     [
-//           { src: '/image1.5e57897a.jpg', alt: 'image src' },
-//           {
-//             src: '/image1.5e57897a.jpg',
-//             'data-src': '/image2.413c237c.jpg',
-//             alt: 'image data-src with lo-fi src',
-//           },
-//         ]

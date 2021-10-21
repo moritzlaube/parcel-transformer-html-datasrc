764ac915913c764f7e5d18044907913400c8ed7e
Moritz Laube <info@moritz-laube.de>
2021-10-21 19:37:46 +0200

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -13,27 +13,27 @@ describe('#parcel()', () => {
     expect(output[0].attrs.src)
       .to.be.a('string')
       .that.matches(/^\/image.[a-f0-9]{8}.(jpg|jpeg|png|svg|webp)$/)
-      .and.equal('/image.5e57897a.jpg')
+      .and.equal('/image.004da3dd.jpg')
   })
 
   it('should hash the "data-src" attribute\'s filename', () => {
     expect(output[1].attrs['data-src'])
       .to.be.a('string')
       .that.matches(/^\/image.[a-f0-9]{8}.(jpg|jpeg|png|svg|webp)$/)
-      .and.equal('/image.5e57897a.jpg')
+      .and.equal('/image.004da3dd.jpg')
   })
 
   it('should hash the "data-srcset" attribute\'s filenames', () => {
     expect(output[2].attrs['data-srcset'])
       .to.be.a('string')
-      .and.equal('/image.5e57897a.jpg 1000w, /image.5e57897a.jpg 2000w')
+      .and.equal('/image.004da3dd.jpg 1000w, /image.004da3dd.jpg 2000w')
   })
 
   it('should hash the "data-background-image" attribute\'s filename', () => {
     expect(output[3].attrs['data-background-image'])
       .to.be.a('string')
       .that.matches(/^\/image.[a-f0-9]{8}.(jpg|jpeg|png|svg|webp)$/)
-      .and.equal('/image.5e57897a.jpg')
+      .and.equal('/image.004da3dd.jpg')
   })
 })
 

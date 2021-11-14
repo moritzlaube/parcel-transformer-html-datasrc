f3c899c9b3e500d4ff63f735b82bc0bcce392485
David Abell <david.abell.m@gmail.com>
2021-11-14 20:59:02 +0000

    add function for combined url and background attrs

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const nullthrows = require('nullthrows')
 const { render } = require('posthtml-render')
 const semver = require('semver')
 const PostHTML = require('posthtml')
+const _ = require('lodash')
+
+// function customizer(objValue, srcValue) {
+//   if (_.isArray(objValue)) {
+//     return objValue.concat(srcValue)
+//   }
+// }
 
 module.exports = new Transformer({
   async loadConfig({ config }) {
@@ -25,7 +32,10 @@ module.exports = new Transformer({
     }
   },
 
-  async transform({ config, asset }) {
+  async transform({ config, asset, logger }) {
+    // must add logger to transform like ({ config, asset, logger}) if compile console.logs are desired
+    // logger.warn({ message: ` log message: ${config}` })
+
     const ast = nullthrows(await asset.getAST())
     let isDirty
     PostHTML().walk.call(ast.program, node => {
@@ -33,41 +43,104 @@ module.exports = new Transformer({
       if (!attrs) {
         return node
       }
+      // Default supported element and attribute pairs
+      const elemAndAttributes = {
+        div: ['data-bg', 'data-background-image', 'data-bg-hidpi', 'data-bg-multi'], // 'data-background-image-set' from lozad neds to be done
+        iframe: ['data-src'],
+        img: ['data-srcset', 'data-src', 'data-bp'],
+        picture: ['data-iesrc'],
+        source: ['data-src', 'data-srcset', 'srcset'],
+        video: ['data-src', 'data-poster'],
+      }
 
-      const customAttrsConfig = Object.keys(config).length === 0 ? undefined : config
+      // Optional user config file
+      const customElemAndAttrsConfig = Object.keys(config).length === 0 ? undefined : config
+      // Custom Attribute values filtered by current tag's attributes
+      const customAttrsValuesFiltered = customElemAndAttrsConfig[tag]
+        ? Object.values(customElemAndAttrsConfig[tag]).filter(item => Object.keys(attrs).includes(item))
+        : undefined
+      // logger.warn({ message: ` customAttrsfiltered: ${customAttrsValuesFiltered}` })
 
-      const customAttr =
-        customAttrsConfig?.[tag] && Array.isArray(customAttrsConfig[tag])
-          ? customAttrsConfig[tag].find(item => Object.keys(attrs).includes(item))
-          : customAttrsConfig?.[tag]
+      const areMultipleFilteredValues = customAttrsValuesFiltered ? /,/.test(customAttrsValuesFiltered) : undefined
+      const testObject = _.merge(elemAndAttributes, customElemAndAttrsConfig)
+      // const secondObject = new Set(testObject)
+      logger.warn({ message: ` testObject: ${Object.entries(testObject)}` })
 
-      // logger.warn({ message: `customAttrTest: ${Object.keys(config)} \n ${Object.keys(attrs)}` })
-      // must add logger to transform like ({ config, asset, logger})
+      // const customAttr =
+      //   customElemAndAttrsConfig[tag] && Array.isArray(customElemAndAttrsConfig[tag])
+      //     ? customElemAndAttrsConfig[tag].find(item => Object.keys(attrs).includes(item))
+      //     : customElemAndAttrsConfig[tag]
+      //     ? customElemAndAttrsConfig[tag]
+      //     : undefined
+      const regexInsideParen = /(?<=url\().*(?=\))/
+      // logger.warn({ message: ` tag: ${tag} \n attrs: ${regexInsideParen.test(Object.entries(attrs))}` })
 
       /*
-      if attrs contains 'url('
-      do...
+       *  Adds url dependency to attributes such as below data-bg-multi that contain
+       *  both multiple url() wrapped sources and other data
+       *
+       *   `<div
+       *     class="lazy"
+       *     data-bg-multi="
+       *       url(lazy-head.jpg),
+       *       url(lazy-body.jpg),
+       *       linear-gradient(#fff, #ccc)"
+       *   >...</div>`
+       *
+       */
+      const addUrlAndGradientAttrsDependency = item => {
+        const srcsets = attrs[item]
+          .trim()
+          .split(',')
+          .map(el => {
+            if (regexInsideParen.test(el)) {
+              const url = el.match(regexInsideParen)
+              const updatedUrl = asset.addURLDependency(url, {})
+              // logger.warn({ message: `\n mapitem: ${el}\n url:${url}\n updatedUrl: ${updatedUrl}`})
+              return el.replace(url, updatedUrl)
+            }
+            return el.trim()
+          })
+          .join(', ')
+        attrs[item] = srcsets
+      }
 
-      if attrs contains ','
-      do...
-
-      if attrs contains ',' && 'url('
-
-
-      */
-      if (customAttrsConfig && tag in customAttrsConfig && attrs[customAttr] != null) {
-        attrs[customAttr] = asset.addURLDependency(attrs[customAttr], {})
+      if (areMultipleFilteredValues) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const item of customAttrsValuesFiltered) {
+          if (attrs[item] != null && regexInsideParen.test(attrs[item])) {
+            addUrlAndGradientAttrsDependency(item)
+          }
+        }
         isDirty = true
       }
 
-      if (tag === 'img' && attrs['data-srcset'] != null) {
-        const srcsets = attrs['data-srcset'].split(',').map(item => {
+      if (tag === 'img' && attrs['data-bp'] != null) {
+        const srcsets = attrs['data-bp'].split(',').map(item => {
           const [url, width] = item.trim().split(' ')
-          return `${asset.addURLDependency(url)} ${width}`
+          return width && url ? `${asset.addURLDependency(url)} ${width}` : asset.addURLDependency(url)
         })
-        attrs['data-srcset'] = srcsets.join(', ')
+        attrs['data-bp'] = srcsets.join(', ')
         isDirty = true
       }
+      // if (
+      //   !areMultipleFilteredValues &&
+      //   customElemAndAttrsConfig &&
+      //   tag in customElemAndAttrsConfig &&
+      //   attrs[customAttrsValuesFiltered] != null
+      // ) {
+      //   attrs[customAttrsValuesFiltered] = asset.addURLDependency(attrs[customAttrsValuesFiltered], {})
+      //   isDirty = true
+      // }
+
+      // if (tag === 'img' && attrs['data-srcset'] != null) {
+      //   const srcsets = attrs['data-srcset'].split(',').map(item => {
+      //     const [url, width] = item.trim().split(' ')
+      //     return `${asset.addURLDependency(url)} ${width}`
+      //   })
+      //   attrs['data-srcset'] = srcsets.join(', ')
+      //   isDirty = true
+      // }
 
       if (tag === 'div' && attrs['data-background-image'] != null) {
         attrs['data-background-image'] = asset.addURLDependency(attrs['data-background-image'], {})
